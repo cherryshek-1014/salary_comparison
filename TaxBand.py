1fade1411a57a8d35edfed338a33e89d089d0260
Cherry Shek <cherryshek.1014@gmail.com>
2021-04-25 17:42:20 +0100

    remove print

--- TaxBand.py
+++ TaxBand.py
@@ -4,6 +4,8 @@ TAX_FREE = 12570
 GLASGOW_BANDS = [12570, 14667, 25296, 43662, 150000]
 GLASGOW_BAND_TAX_RATES = [0.19, 0.20, 0.21, 0.41, 0.46]
 LONDON_BANDS = [12570, 14667, 25296, 43662, 150000]
+
+
 class TAX_BAND(Enum):
     STARTER = 0
     BASIC = 1
@@ -19,7 +21,7 @@ class TaxCalculator:
         total = 0
         if self.salary <= TAX_FREE:
             return 0
-        
+
         for index, value in enumerate(GLASGOW_BANDS):
             if index >= len(GLASGOW_BANDS) - 1:
                 break
@@ -27,8 +29,7 @@ class TaxCalculator:
                 taxable_amount_in_band = (GLASGOW_BANDS[index + 1] - value)
             else:
                 taxable_amount_in_band = (self.salary - value)
-            
+
             total += taxable_amount_in_band * GLASGOW_BAND_TAX_RATES[index]
 
-        print(total)
         return total
